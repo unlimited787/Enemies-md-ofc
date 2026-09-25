f15dd7dd85cea2f6c8e81a1a6988f0cdccab44e8
unlimitedilvero <115047957+unlimited787@users.noreply.github.com>
2026-09-25 12:51:30 +0200

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,23 +1,117 @@
 import { addExif } from '../lib/sticker.js'
+
 let handler = async (m, { conn, text }) => {
-if (!m.quoted) return
-let stiker = false
-try {
-let [packname, ...author] = text.split('|')
-author = (author || []).join('|')
-let mime = m.quoted.mimetype || ''
-if (!/webp/.test(mime)) return
-let img = await m.quoted.download()
-if (!img) return
-stiker = await addExif(img, packname || '', author || '')
-} catch (e) {
-console.error(e)
-if (Buffer.isBuffer(e)) stiker = e
-} finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
-else throw '𝐞𝐫𝐫𝐨𝐫𝐞 ✗'
-}}
-handler.help = ['wm <packname>|<author>']
-handler.tags = ['sticker']
-handler.command = /^robar|wm$/i
+    if (!m.quoted) return
+
+    let stiker = false
+
+    try {
+        let [packname, ...author] = (text || '').split('|')
+
+        author = author.join('|')
+
+        const mime =
+            m.quoted.mimetype ||
+            m.quoted.msg?.mimetype ||
+            ''
+
+        if (!/webp/i.test(mime)) return
+
+ 
+
+        const msg =
+            m.quoted.msg ||
+            m.quoted
+
+        let img
+
+        try {
+            img = await conn.downloadM(
+                msg,
+                'sticker',
+                false
+            )
+        } catch (e) {
+            console.error(
+                'WM downloadM ERROR:',
+                e?.stack || e
+            )
+        }
+
+        if (
+            !img ||
+            !Buffer.isBuffer(img) ||
+            !img.length
+        ) {
+            throw new Error(
+                'Impossibile scaricare lo sticker WebP'
+            )
+        }
+
+        console.log(
+            'WM: WebP scaricato:',
+            img.length,
+            'bytes'
+        )
+
+        /*
+         * AGGIUNTA EXIF
+         */
+
+        stiker = await addExif(
+            img,
+            packname || '',
+            author || ''
+        )
+
+    } catch (e) {
+
+        console.error(
+            'WM ERROR:',
+            e?.stack || e
+        )
+    }
+
+
+    if (stiker) {
+
+        try {
+
+            await conn.sendFile(
+                m.chat,
+                stiker,
+                'wm.webp',
+                '',
+                m,
+                false,
+                {
+                    asSticker: true
+                }
+            )
+
+        } catch (e) {
+
+            console.error(
+                'WM SEND ERROR:',
+                e?.stack || e
+            )
+        }
+
+    } else {
+
+        throw '𝐞𝐫𝐫𝐨𝐫𝐞 ✗'
+    }
+}
+
+handler.help = [
+    'wm <packname>|<author>'
+]
+
+handler.tags = [
+    'sticker'
+]
+
+handler.command =
+    /^robar|wm$/i
+
 export default handler
