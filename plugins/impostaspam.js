566acf50d719fde92e6aafd44110dd53a9a86a99
unlimitedilvero <115047957+unlimited787@users.noreply.github.com>
2026-09-25 11:45:59 +0200

    Add files via upload

---
+++ plugins/impostaspam.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, text }) => {
+  if (!text?.trim()) {
+    return m.reply('e allora?')
+  }
+
+  const link = text.trim()
+
+  global.spamLink = link
+
+  return m.reply(`fatto! preparatevi gente!`)
+}
+
+handler.command = ['impostaspam']
+handler.help = ['impostaspam <link>']
+handler.tags = ['owner']
+
+export default handler
