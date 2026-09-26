855919ecd7ebd4bdf4a8ce3434e137dfe683b4c0
unlimitedilvero <115047957+unlimited787@users.noreply.github.com>
2026-09-26 16:52:36 +0200

    Refactor menu.js to improve code structure

--- plugins/menu.js
+++ plugins/menu.js
@@ -113,7 +113,7 @@ let info = `
 │⭔ ${usedPrefix}abbraccio @
 │⭔ ${usedPrefix}amore @
 │⭔ ${usedPrefix}andre (AI)
-│⭔ ${usedPrefix}wikipedia (txt)⚠️
+│⭔ ${usedPrefix}wikipedia 
 │⭔ ${usedPrefix}dox @
 │⭔ ${usedPrefix}topgays
 │⭔ ${usedPrefix}topnazi
