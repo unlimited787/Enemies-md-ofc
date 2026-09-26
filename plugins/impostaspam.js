410c532a5f53ac70e1f5dc55eca162aa9cac5343
unlimitedilvero <115047957+unlimited787@users.noreply.github.com>
2026-09-26 16:32:30 +0200

    Change handler.tags to handler.owner for impostaspam

--- plugins/impostaspam.js
+++ plugins/impostaspam.js
@@ -12,6 +12,6 @@ let handler = async (m, { conn, text }) => {
 
 handler.command = ['impostaspam']
 handler.help = ['impostaspam <link>']
-handler.tags = ['owner']
+handler.owner = true
 
 export default handler
